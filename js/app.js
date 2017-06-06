7582e3580166ef9cfa4b67dc6ae3f38fb1f7bfa8
Erica Wright <ericaw21@gmail.com>
2017-06-06 14:07:14 -0700

    attempt api call through Vue

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ Vue.component('table-component', {
         template: '<p class="table">This is a table</p>'
     })
 
+Vue.component('api-component', {
+    template: '<p class="api">The weather is: </p>'
+})
+
 window.onload = function() {
     var header = new Vue({
         el: '#header',
@@ -16,7 +20,25 @@ window.onload = function() {
             message: 'Vue has paragraphs'
         }
     })
+
     var piece = new Vue({
         el: '#table'
+    })
+
+    var api = new Vue({
+        el: '#api',
+        data() {
+            return {
+                result: ''
+            }
+        },
+        methods: {
+            showWeather: function() {
+                this.http.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=742bde82c919119ade40d7d9879ca90e', (data) => {
+                    this.result = data;
+                })
+                .error((err) => console.log(err))
+            }
+        }
     })
 }
